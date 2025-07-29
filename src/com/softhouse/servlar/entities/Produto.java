75631911a7402ade52bf118e5e3cb7fa1c910620
Ricardo de Camargo <139239844+RicardoCamargoPS@users.noreply.github.com>
2025-07-29 19:47:54 -0300

    criado  orientação para desenvolvimento do sistema

--- src/com/softhouse/servlar/entities/Produto.java
+++ src/com/softhouse/servlar/entities/Produto.java
@@ -2,4 +2,11 @@ package com.softhouse.servlar.entities;
 
 public class Produto {
 
+    private int codigoSKU;
+    private int codigoEAN13;
+    private String nome;
+    private String descricao
+
+    
+
 }
