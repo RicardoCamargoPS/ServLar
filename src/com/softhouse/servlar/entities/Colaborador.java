d51ca4c18e2625321e3c3b032987dddaa2a3ca7d
Ricardo de Camargo <139239844+RicardoCamargoPS@users.noreply.github.com>
2025-08-02 15:46:03 -0300

    implementado classes Colaborador, Fornecedor e Insumos

--- src/com/softhouse/servlar/entities/Colaborador.java
+++ src/com/softhouse/servlar/entities/Colaborador.java
@@ -2,4 +2,88 @@ package com.softhouse.servlar.entities;
 
 public class Colaborador {
 
+    private String nome;
+    private int RG, CPF, tempoServico;
+    private String funcao, setor, turno;
+    private String dataContratacao, dataNascimento;
+    private boolean temFeriasVencida;
+    private double salario;
+    private boolean usuarioSistema;
+
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getRG() {
+        return RG;
+    }
+    public void setRG(int rG) {
+        RG = rG;
+    }
+    public int getCPF() {
+        return CPF;
+    }
+    public void setCPF(int cPF) {
+        CPF = cPF;
+    }
+    public int getTempoServico() {
+        return tempoServico;
+    }
+    public void setTempoServico(int tempoServico) {
+        this.tempoServico = tempoServico;
+    }
+    public String getFuncao() {
+        return funcao;
+    }
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+    public String getSetor() {
+        return setor;
+    }
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+    public String getTurno() {
+        return turno;
+    }
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
+    public String getDataContratacao() {
+        return dataContratacao;
+    }
+    public void setDataContratacao(String dataContratacao) {
+        this.dataContratacao = dataContratacao;
+    }
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+    public boolean isTemFeriasVencida() {
+        return temFeriasVencida;
+    }
+    public void setTemFeriasVencida(boolean temFeriasVencida) {
+        this.temFeriasVencida = temFeriasVencida;
+    }
+    public double getSalario() {
+        return salario;
+    }
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    public boolean isUsuarioSistema() {
+        return usuarioSistema;
+    }
+    public void setUsuarioSistema(boolean usuarioSistema) {
+        this.usuarioSistema = usuarioSistema;
+    }
+
+    
+
 }
--- src/com/softhouse/servlar/entities/Insumos.java
+++ src/com/softhouse/servlar/entities/Insumos.java
@@ -2,4 +2,82 @@ package com.softhouse.servlar.entities;
 
 public class Insumos {
 
+    private String nome, descricao, unidadeMedida;
+    private int codigo;     
+    private long precoCusto;
+    private boolean fracionamento, disponivel;
+    private String categoria, subCategoria, marca;
+    private String fornecedor;
+
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public String getUnidadeMedida() {
+        return unidadeMedida;
+    }
+    public void setUnidadeMedida(String unidadeMedida) {
+        this.unidadeMedida = unidadeMedida;
+    }
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    public long getPrecoCusto() {
+        return precoCusto;
+    }
+    public void setPrecoCusto(long precoCusto) {
+        this.precoCusto = precoCusto;
+    }
+    public boolean isFracionamento() {
+        return fracionamento;
+    }
+    public void setFracionamento(boolean fracionamento) {
+        this.fracionamento = fracionamento;
+    }
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+    public String getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    public String getSubCategoria() {
+        return subCategoria;
+    }
+    public void setSubCategoria(String subCategoria) {
+        this.subCategoria = subCategoria;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public String getFornecedor() {
+        return fornecedor;
+    }
+    public void setFornecedor(String fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+
+
+    
 }
