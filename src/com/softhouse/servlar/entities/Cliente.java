714fe0bf21cf40ea013b435ae8ef0b6dfb43b383
Ricardo de Camargo <139239844+RicardoCamargoPS@users.noreply.github.com>
2025-08-28 19:08:16 -0300

    criado class Cliente

--- src/com/softhouse/servlar/entities/Cliente.java
+++ src/com/softhouse/servlar/entities/Cliente.java
@@ -2,4 +2,60 @@ package com.softhouse.servlar.entities;
 
 public class Cliente {
 
+    private String nome;
+    private int CNPJ, telefone;
+    private String nomeRepresentante;
+    private String segmento;
+    private boolean bloqueado;
+    private double saldo;
+
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getCNPJ() {
+        return CNPJ;
+    }
+    public void setCNPJ(int cNPJ) {
+        CNPJ = cNPJ;
+    }
+    public int getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+    public String getNomeRepresentante() {
+        return nomeRepresentante;
+    }
+    public void setNomeRepresentante(String nomeRepresentante) {
+        this.nomeRepresentante = nomeRepresentante;
+    }
+    public String getSegmento() {
+        return segmento;
+    }
+    public void setSegmento(String segmento) {
+        this.segmento = segmento;
+    }
+    public boolean isBloqueado() {
+        return bloqueado;
+    }
+    public void setBloqueado(boolean bloqueado) {
+        this.bloqueado = bloqueado;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    
+
+
+
+
 }
